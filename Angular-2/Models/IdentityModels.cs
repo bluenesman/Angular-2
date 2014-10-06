f72f6f95259f9015770e54addd2ed91b9a2739b3
bluenesman <bluenesman@gmail.com>
2014-10-06 17:59:05 +0300

    web.release.config db transform

--- Angular-2/Models/IdentityModels.cs
+++ Angular-2/Models/IdentityModels.cs
@@ -22,7 +22,7 @@ namespace Angular_2.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("TodoContext", throwIfV1Schema: false)
         {
         }
         
